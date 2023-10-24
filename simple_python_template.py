44a24b0e1fee9466f00de7159647b17db4b411c4
Dylan McClarence <20111795@tafe.wa.edu.au>
2023-10-24 17:08:45 +0800

    Added ability to store player statistics and calculate player's average number of guesses

--- simple_python_template.py
+++ simple_python_template.py
@@ -54,6 +54,18 @@ def pick_target_word(target_words):
     return random.choice(target_words)
 
 
+def get_player_name():
+    player = ""
+    while player == "" or player.find(" ") != -1:
+        player = input("Enter Player Name: ")
+        print()
+        if player == "" or player.find(" ") != -1:
+            print("Please enter a Name.")
+            print()
+
+    return player
+
+
 def validate_guess(guess, word_list):
     """
     Validates user input by checking if the word exists in a file
@@ -188,29 +200,99 @@ def format_score(guess, hint):
           f"Hint:  {' '.join(str(letter) for letter in formatted_hint)}\n")
 
 
+def open_statistics_file_for_editing():
+    """
+    Opens statistics.csv for modification, creating the file if it does not exist
+
+        :returns: file object
+    """
+
+    try:
+        statistics = open("statistics.csv", 'a')
+    except:
+        statistics = open("statistics.csv", 'x')
+
+    return statistics
+
+
+def store_player_statistics(name, attempts, target_word, hint):
+    """
+        Writes the players data to the statistics.csv file
+
+        :param name: string
+        :param attempts: int
+        :param target_word: string
+        :param hint: tuple
+    """
+
+    statistics = open_statistics_file_for_editing()
+    statistics.write(f"Name: {name.upper()}, "
+                     f"Attempts to Correct Guess: {attempts if is_correct(hint) else "unsuccessful"}, "
+                     f"Target: {target_word}\n")
+    statistics.close()
+
+
+def average_guesses_for_player(name):
+    """
+        Calculates the average number of guesses for a player
+
+        :param statistics: file object
+        :param name: string
+    """
+
+    statistics = open("statistics.csv")
+
+    count = 0
+    total_number_of_attempts = 0
+
+    for line in statistics:
+        name_start = line.find(':') + 2
+        name_end = line.find(',')
+        if line[name_start:name_end] == name.upper():
+            attempts = line.find(":", name_end) + 2
+            try:
+                total_number_of_attempts += int(line[attempts])
+            except:
+                total_number_of_attempts += MAX_TRIES
+            count += 1
+
+    average = total_number_of_attempts / count
+
+    statistics.close()
+
+    return average
+
+
 def game_loop():
     target_word_bank = open_word_bank(TARGET_WORDS)
     valid_word_bank = open_word_bank(VALID_WORDS)
 
     target_word = pick_target_word(target_word_bank)
 
+    player = get_player_name()
+
     attempts = 0  # Count for the players attempts
     while attempts < MAX_TRIES:
-        guess = input(f"Enter guess? (Cheat: {target_word})\n").strip().lower()
+        guess = input(f"Enter guess? \n").strip().lower()
         if validate_guess(guess, valid_word_bank):
             hint = score_guess(target_word, guess)
             if is_correct(hint):
                 print(f"\nYour guess, {guess.upper()}, is correct!\n")
+                attempts += 1
                 break
             else:
                 print("\nYour guess is wrong!\n")
                 format_score(guess, hint)
-            attempts += 1
+                attempts += 1
         else:
             print(f"\n{guess.upper()} is not a valid word. Please Try Again\n")
-    if attempts == MAX_TRIES:
+    if attempts == MAX_TRIES and not is_correct(hint):
         print(f"The word was {target_word.upper()}.")
-    print("Game Over")
+    print("Game Over\n")
+
+    store_player_statistics(player, attempts, target_word, hint)
+    average = average_guesses_for_player(player)
+    print(f"The average amount of guesses per target word for {player.upper()} is {average}")
 
 
 def main(test=False):
